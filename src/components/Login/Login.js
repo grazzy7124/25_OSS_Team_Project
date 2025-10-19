0e82c26cb10b6b822ecd21c0ea33b5e7de1cc9b0
grazzy <grazzy7124@gmail.com>
2025-10-19 23:53:02 +0900

    로그인 UI 수정

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,15 +6,19 @@ const Login = ()=> {
     return (
         
         <>
-            <div className="container justify-content-center">
+            <div className="container d-flex flex-column justify-content-center align-items-center vh-100">
+                <h2 className="text-center mb-3">Login</h2>
                 <div>
-                    <input placeholder="email"></input>
+                    <input className='form-control mb-2' placeholder="email"></input>
                 </div>
                 <div>
-                    <input placeholder="password"></input>
+                    <input className='form-control mb-3' placeholder="password"></input>
                 </div>
                 <div>
-                    <button onClick={() => navigate('/main')}>
+                    <button 
+                        className='btn btn-primary'
+                        onClick={() => navigate('/main')}
+                    >
                         Submit
                     </button>
                 </div>
